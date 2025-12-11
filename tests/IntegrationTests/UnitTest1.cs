d7d359c4d90fe2d80ff579a66db03a945ec1e5f0
Samuel Ogboye <ogboyesam@gmail.com>
2025-12-11 02:07:58 +0100

    feat: add initial unit tests for integration and unit test classes

---
+++ tests/IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PayWarden.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PayWarden.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
